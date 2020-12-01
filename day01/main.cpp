cd5e27bc9cafe63c1f3b5f9da308bd410bccc2a4
Mauri Mustonen <mauri_mustonen@hotmail.com>
2020-12-01 12:27:21 +0200

    second solution

--- day01/main.cpp
+++ day01/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <cstdint>
+#include <cassert>
 
 void read_file(std::vector<std::uint32_t> &buffer) {
     std::string line;
@@ -10,21 +11,46 @@ void read_file(std::vector<std::uint32_t> &buffer) {
     }
 }
 
-int main() {
-    std::vector<std::uint32_t> input = {};
+int first_solution(std::vector<std::uint32_t> &input) {
+    int target = 2020;
     std::vector<std::uint32_t>::iterator main_it;
     std::vector<std::uint32_t>::iterator second_it;
-    int target = 2020;
-
-    read_file(input);
-
     for (main_it = input.begin(); main_it != input.end(); ++main_it) {
         for (second_it = std::next(main_it, 1); second_it != input.end(); ++second_it) {
             if (*main_it + *second_it == target) {
-                std::cout << "answer: " << *main_it * *second_it << std::endl;
-                return 0;
+                return *main_it * *second_it;
             }
         }
     }
-    return 1;
+    return 0;
+}
+
+int second_solution(std::vector<std::uint32_t> &input) {
+    int target = 2020;
+    std::vector<std::uint32_t>::iterator main_it;
+    std::vector<std::uint32_t>::iterator second_it;
+    std::vector<std::uint32_t>::iterator third_it;
+    for (main_it = input.begin(); main_it != input.end(); ++main_it) {
+        for (second_it = std::next(main_it, 1); second_it != input.end(); ++second_it) {
+            for (third_it = std::next(second_it, 1); third_it != input.end(); ++third_it) {
+                if (*main_it + *second_it + *third_it == target) {
+                    return *main_it * *second_it * *third_it;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+    std::vector<std::uint32_t> input = {};
+
+    read_file(input);
+    std::uint32_t first = first_solution(input);
+    std::uint32_t second = second_solution(input);
+    assert(first == 988771 && "first solution doesn't match");
+    assert(second == 171933104 && "second solution doesn't match");
+    std::cout << "first answer: " << first << std::endl;
+    std::cout << "second answer: " << second << std::endl;
+    return 0;
 }
