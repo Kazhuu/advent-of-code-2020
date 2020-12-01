92350e223c35aeff5db7c786d2f0ccf14bf7112a
Mauri Mustonen <mauri_mustonen@hotmail.com>
2020-12-01 10:44:49 +0200

    part one

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,10 +1,30 @@
 #include <string>
 #include <iostream>
+#include <vector>
+#include <cstdint>
 
-int main() {
+void read_file(std::vector<std::uint32_t> &buffer) {
     std::string line;
     while (std::getline(std::cin, line)) {
-        std::cout << line << std::endl;
+        buffer.push_back(std::stoi(line));
     }
-    return 0;
+}
+
+int main() {
+    std::vector<std::uint32_t> input = {};
+    std::vector<std::uint32_t>::iterator main_it;
+    std::vector<std::uint32_t>::iterator second_it;
+    int target = 2020;
+
+    read_file(input);
+
+    for (main_it = input.begin(); main_it != input.end(); ++main_it) {
+        for (second_it = std::next(main_it, 1); second_it != input.end(); ++second_it) {
+            if (*main_it + *second_it == target) {
+                std::cout << "answer: " << *main_it * *second_it << std::endl;
+                return 0;
+            }
+        }
+    }
+    return 1;
 }
