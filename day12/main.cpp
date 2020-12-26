111a6721bae07c8fc2b9ae34265d34d404417cf4
Mauri Mustonen <mauri_mustonen@hotmail.com>
2020-12-26 21:41:20 +0200

    use complex numbers for simpler solution

--- day12/main.cpp
+++ day12/main.cpp
@@ -4,7 +4,23 @@
 #include <string>
 #include <iostream>
 #include <cassert>
+#include <complex>
+#include <unordered_map>
 #include <tuple>
+
+using namespace std::complex_literals;
+
+std::unordered_map<char, std::complex<double>> deltas{
+    {'N', 0. + 1i},
+    {'S', 0. - 1i},
+    {'E', 1. + 0i},
+    {'W', -1. + 0i}
+};
+
+std::unordered_map<char, int> rotation{
+    {'L', 1},
+    {'R', -1}
+};
 
 std::vector<std::string> read_stdin() {
     std::vector<std::string> input(
@@ -13,104 +29,48 @@ std::vector<std::string> read_stdin() {
     return input;
 }
 
-std::tuple<char, int64_t> parse_line(const std::string &line) {
+std::tuple<char, double> parse_line(const std::string &line) {
     return std::make_tuple(line[0], std::stol(line.substr(1, line.size())));
 }
 
-std::pair<int64_t, int64_t> rotate(int64_t x, int64_t y, char direction, int64_t degrees) {
-    int64_t count = degrees / 90;
-    for (int i = 0; i < count; ++i) {
-        std::swap(x, y);
-        if (direction == 'R') {
-            y = -y;
-        } else  {
-            x = -x;
-        }
-    }
-    return std::make_pair(x, y);
-}
-
 uint64_t first_solution(const std::vector<std::string> &input) {
-    int64_t rotation = 0;
-    int64_t x = 0;
-    int64_t y = 0;
+    std::complex<double> ship = 0. + 0i;
+    std::complex<double> delta = deltas['E'];
     for (auto &line : input) {
-        auto [direction, value] = parse_line(line);
+        auto [direction, n] = parse_line(line);
         switch (direction) {
-            case 'N':
-                y += value;
-                break;
-            case 'S':
-                y -= value;
-                break;
-            case 'E':
-                x += value;
-                break;
-            case 'W':
-                x -= value;
-                break;
             case 'L':
-                rotation = (rotation - value) % 360;
-                break;
             case 'R':
-                rotation = (rotation + value) % 360;
+                delta *= std::pow(1i, (rotation[direction] * (int)n) / 90);
                 break;
             case 'F':
-                if (rotation == 0) {
-                    x += value;
-                } else if (rotation == 90 || rotation == -270) {
-                    y -= value;
-                } else if (rotation == 180 || rotation == -180) {
-                    x -= value;
-                } else if (rotation == 270 || rotation == -90) {
-                    y += value;
-                } else {
-                    std::cout << "wrong degree" << std::endl;
-                    exit(1);
-                }
+                ship += delta * n;
                 break;
             default:
-                std::cout << "wrong command" << std::endl;
-                exit(1);
+                ship += deltas[direction] * n;
         }
     }
-    return std::abs(x) + std::abs(y);
+    return std::abs(ship.real()) + std::abs(ship.imag());
 }
 
 uint64_t second_solution(const std::vector<std::string> &input) {
-    int64_t way_point_x = 10;
-    int64_t way_point_y = 1;
-    int64_t ship_x = 0;
-    int64_t ship_y = 0;
+    std::complex<double> ship = 0. + 0i;
+    std::complex<double> waypoint = 10. + 1i;
     for (auto &line : input) {
-        auto [direction, value] = parse_line(line);
+        auto [direction, n] = parse_line(line);
         switch (direction) {
-            case 'N':
-                way_point_y += value;
-                break;
-            case 'S':
-                way_point_y -= value;
-                break;
-            case 'E':
-                way_point_x += value;
-                break;
-            case 'W':
-                way_point_x -= value;
-                break;
             case 'L':
             case 'R':
-                std::tie(way_point_x, way_point_y) = rotate(way_point_x, way_point_y, direction, value);
+                waypoint *= std::pow(1i, (rotation[direction] * (int)n) / 90);
                 break;
             case 'F':
-                ship_x += value * way_point_x;
-                ship_y += value * way_point_y;
+                ship += waypoint * n;
                 break;
             default:
-                std::cout << "wrong command" << std::endl;
-                exit(1);
+                waypoint += deltas[direction] * n;
         }
     }
-    return std::abs(ship_x) + std::abs(ship_y);
+    return std::abs(ship.real()) + std::abs(ship.imag());
 }
 
 int main() {
