5a19d9063045b9510b440a1421ea89c6b585c1c5
Mauri Mustonen <mauri_mustonen@hotmail.com>
2021-10-10 11:40:35 +0300

    second solution

--- day21/main.cpp
+++ day21/main.cpp
@@ -5,6 +5,9 @@
 #include <set>
 #include <iterator>
 #include <cassert>
+#include <algorithm>
+#include <map>
+#include <sstream>
 
 std::vector<std::string> read_stdin() {
     std::vector<std::string> input;
@@ -15,10 +18,18 @@ std::vector<std::string> read_stdin() {
     return input;
 }
 
+void remove_ingredient(const std::string &ingredient, std::map<std::string, std::set<std::string>> &allergens) {
+    for (auto &allergen_pair : allergens) {
+        if (allergen_pair.second.size() > 1) {
+            allergen_pair.second.erase(ingredient);
+        }
+    }
+}
+
 int main() {
     std::vector<std::string> input = read_stdin();
     std::multiset<std::string> all_ingredients;
-    std::unordered_map<std::string, std::set<std::string>> allergens;
+    std::map<std::string, std::set<std::string>> allergens;
 
     std::regex word_pattern(R"(\b\w+\b)");
     std::set<std::string> current_ingredients;
@@ -53,6 +64,8 @@ int main() {
             }
         }
     }
+
+    // Part 1.
     for (auto &allergen : allergens) {
         for (auto &ingredient : allergen.second) {
             all_ingredients.erase(ingredient);
@@ -61,5 +74,22 @@ int main() {
     uint64_t first = all_ingredients.size();
     std::cout << "First answer: " << first << std::endl;
     assert(first == 2874 && "first solution doesn't match");
+
+    // Part 2.
+    while(std::any_of(std::begin(allergens), std::end(allergens),
+                     [](auto &allergen_pair) { return allergen_pair.second.size() > 1; })) {
+        for (auto &allergen_pair : allergens) {
+            if (allergen_pair.second.size() == 1) {
+                std::string ingredient = *std::begin(allergen_pair.second);
+                remove_ingredient(ingredient, allergens);
+            }
+        }
+    }
+    std::stringstream dangerous_list;
+    for (auto &allergen : allergens) {
+        dangerous_list << *std::begin(allergen.second) << ",";
+    }
+    std::cout << "second answer: " << dangerous_list.str() << std::endl;
+    assert(dangerous_list.str() == "gfvrr,ndkkq,jxcxh,bthjz,sgzr,mbkbn,pkkg,mjbtz," && "second solution doesn't match");
     return 0;
 }
